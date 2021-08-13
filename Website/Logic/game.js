c2028e54f9aed31014ed379b9f055975454c7e66
Zhenye-pzy <mrbean1622@gmail.com>
2021-08-13 11:09:09 +0800

    shifting log folder into website

--- Website/Logic/game.js
+++ Website/Logic/game.js
@@ -3,7 +3,7 @@ import { update as updateFood, draw as drawFood } from './snakefood.js';
 import { outsideGrid } from './Grids.js'
 
 
-let host = 'http://localhost:8800'
+// let host = 'http://localhost:8800'
 //RenderTime starts from 0
 let lastRenderTime = 0;
 let GameOver = false
@@ -72,43 +72,43 @@ function checkDeath() {
 
 }
 
-var Names = localStorage.getItem('Names');
-var Score = localStorage.getItem('Score');
+// var Names = localStorage.getItem('Names');
+// var Score = localStorage.getItem('Score');
 
-fetch(`${host}/Gamestats?player=${Names}&Score=${Score}`,
-    { method: "POST" })
-    .then((response) => {
-        return response.json()
-    }).then((json) => {
-        var html = `
-        <div class="card" style="direction: flex; opacity: 85%">
+// fetch(`${host}/Gamestats?player=${Names}&Score=${Score}`,
+//     { method: "POST" })
+//     .then((response) => {
+//         return response.json()
+//     }).then((json) => {
+//         var html = `
+//         <div class="card" style="direction: flex; opacity: 85%">
 
-            <div class="card-header" style="background-color: #F77E01; height: 40px; border-top-left-radius: 12px; border-top-right-radius: 12px;">
-                <p class="card-text" style="padding: 0.8em; padding-left: 1.5em">#${Names}</p>
-            </div>
+//             <div class="card-header" style="background-color: #F77E01; height: 40px; border-top-left-radius: 12px; border-top-right-radius: 12px;">
+//                 <p class="card-text" style="padding: 0.8em; padding-left: 1.5em">#${Names}</p>
+//             </div>
 
-            <div class="card-body" style="background-color: white; padding: 1em; 
-            overflow: hidden; text-overflow: ellipsis; max-width: 215px; min-height: 120px; max-height: 120px; word-break: break-all;
-            border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;">
+//             <div class="card-body" style="background-color: white; padding: 1em; 
+//             overflow: hidden; text-overflow: ellipsis; max-width: 215px; min-height: 120px; max-height: 120px; word-break: break-all;
+//             border-bottom-left-radius: 12px; border-bottom-right-radius: 12px;">
 
-                <p class="card-text" style="color: black; overflow: hidden; max-height: 55px; margin-bottom: 1.5em">${Score}</p>
+//                 <p class="card-text" style="color: black; overflow: hidden; max-height: 55px; margin-bottom: 1.5em">${Score}</p>
                 
-                <div style="min-height: 20px;">
-                    <a href="owner.html" style="    
-                    padding: 7px 12px;
-                    text-decoration: none;
-                    color: white;
-                    background-color: #BC80F7;
-                    border-radius: 0.5em;
-                    border-top: 4px solid #BC80F7;
-                    border-left: 4px solid #BC80F7;
-                    border-bottom: 4px solid #744a9e;
-                    border-right: 4px solid #744a9e;">View</a>
-                </div>
+//                 <div style="min-height: 20px;">
+//                     <a href="owner.html" style="    
+//                     padding: 7px 12px;
+//                     text-decoration: none;
+//                     color: white;
+//                     background-color: #BC80F7;
+//                     border-radius: 0.5em;
+//                     border-top: 4px solid #BC80F7;
+//                     border-left: 4px solid #BC80F7;
+//                     border-bottom: 4px solid #744a9e;
+//                     border-right: 4px solid #744a9e;">View</a>
+//                 </div>
 
-            </div>
-        </div>
-            `;
-            $('#stats-display').append(html);
-    })
+//             </div>
+//         </div>
+//             `;
+//             $('#stats-display').append(html);
+//     })
 // })
